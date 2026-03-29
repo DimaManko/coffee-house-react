be0b09e3bec4225717fabb073f8510f1b92d6953
DimaManko <mankonat@gmail.com>
2026-03-29 17:13:20 +0300

    feat: add about section

---
+++ src/components/about/about.jsx
@@ -0,0 +1,23 @@
+import BeansDivider from "../beans-divider/beans-divider";
+import './about.scss'
+
+const About = () => {
+    return (
+        <section className="about">
+            <h2 className="about__subtitle">About Us</h2>
+            <BeansDivider color="black"/>
+            <p className="about__text">Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+Afraid at highly months do things on at. Situation recommend objection do intention
+so questions. As greatly removed calling pleased improve an. Last ask him cold feel
+met spot shy want. Children me laughing we prospect answered followed. At it went
+is song that held help face.</p>
+            <p className="about__text">Now residence dashwoods she excellent you. Shade being under his bed her, Much
+read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
+horrible but confined day end marriage. Eagerness furniture set preserved far
+recommend. Did even but nor are most gave hope. Secure active living depend son
+repair day ladies now.</p>
+        </section>
+    )
+}
+
+export default About;
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react"
 import Header from "../app-header/app-header";
 import Promo from "../promo/promo";
+import About from "../about/about";
 
 class App extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class App extends Component {
       <div>
         <Header/>
         <Promo/>
+        <About/>
       </div>
     )
   }
