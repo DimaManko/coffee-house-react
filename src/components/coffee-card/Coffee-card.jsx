28d65b821f4b2a6d8fc747b897abd1133eca9e8b
DimaManko <mankonat@gmail.com>
2026-04-07 20:03:22 +0300

    fix: style coffee card

--- src/components/coffee-card/Coffee-card.jsx
+++ src/components/coffee-card/Coffee-card.jsx
@@ -4,7 +4,9 @@ const CoffeeCard = ({name, price, src, country, bestseller}) => {
     const clazz = `coffee-item ${bestseller ? 'coffee-item_best' : ''}` 
     return (
         <div className={clazz}>
-            <img src={src} alt={name} className='coffee-item__img'/>
+            <div className="coffee-item__img-wrapper">
+                <img src={src} alt={name} className='coffee-item__img'/>
+            </div>
             <div className='coffee-item__name'>{name}</div>
             {country && !bestseller ? <div className="coffee-item__country">{country}</div> : null}
             <div className="coffee-item__price">{price}</div>
