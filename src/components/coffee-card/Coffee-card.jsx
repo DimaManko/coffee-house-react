2575de007822b2a99c2bf377e81834c85b0040b9
DimaManko <mankonat@gmail.com>
2026-04-12 15:16:54 +0300

    style: coffee-card

--- src/components/coffee-card/Coffee-card.jsx
+++ src/components/coffee-card/Coffee-card.jsx
@@ -7,9 +7,11 @@ const CoffeeCard = ({name, price, src, country, bestseller}) => {
             <div className="coffee-item__img-wrapper">
                 <img src={src} alt={name} className='coffee-item__img'/>
             </div>
-            <div className='coffee-item__name'>{name}</div>
-            {country && !bestseller ? <div className="coffee-item__country">{country}</div> : null}
-            <div className="coffee-item__price">{price}</div>
+            <div className="coffee-item__info">
+                <div className='coffee-item__name'>{name}</div>
+                {country && !bestseller ? <div className="coffee-item__country">{country}</div> : null}
+                <div className="coffee-item__price">{price}</div>
+            </div>
         </div>
     )
 }
